5f0c2785fe0f0bde027a035f6d10264df58c9a97
Sabiq Sabry <sabiqsabry48@gmail.com>
2023-07-24 10:30:09 +0100

    files

---
+++ UberClone/App/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Sabiq Sabry on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberClone/Core/Home/Views/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Sabiq Sabry on 13/07/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            LocationSearchActivationView()
+                .padding(.top, 72)
+            
+            MapViewActionButton()
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
